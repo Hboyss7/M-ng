cd1aae3364fa1bb1cdf03455b489461c740319d0
Hboyss7 <150256806+Hboyss7@users.noreply.github.com>
2024-09-13 09:15:44 +0700

    Add files via upload

---
+++ M-ng-main/2212366_BTHieu_Lab4/ListenerEmployee/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListenerEmployee
+{
+    internal class Employee
+    {
+        public int IDEmployee;
+
+        public string LastName;
+
+        public string FirstName;
+
+        public int YearService;
+
+        public double Salary;
+
+        public int LastNameSize;
+
+        public int FirstNameSize;
+
+        public int Size;
+    }
+
+    // GetByte :   Dùng Phương Thức GetBytes() của lớp BitConverter 
+    //             _ Trả vể Mảng Byte
+    //             _BitConverter.GetBytes(Varible) : int, double,..
+    //             _Encoding.ASCII.GetBytes(Varible) : string
+    //             Dùng phương thức BlockCopy của lớp Butter 
+    //             _ Để tập hơp các mảng byte thành phần vào 1 mảng lớn
+    //             _ Butter.BlockCopy(ArrayByte[] ThànhPhầni, int start,
+    //                 ArrayByte[] tập hợp, int SizeByte]
+    //             _Size cần lấy trong ArrayByte là Byte
+    //             _Ta sẽ căn cứ vào Length của Varible để lấy được Size (string)
+    //             _  hoặc là số Byte của kiểu dữ liệu để lấy được chính xác hơn
+}   // 
+    //              
+    // ConvertByte: Dùng phương thức các Converter của BitConverter ( ToInt32(), ToDouble()) 
+    //              - Encoding.ASCII.GetString(ArrayByte[] tập hợp, place, size) : string
+    //              _ BitConverter.ToInt32(ArrayByte[] tập hợp, place)
+    //         Không cần size vì đã biết số Byte của các phương thức Convert ( Int32 = 4 byte)
+    //              _ StringSize sẽ không cố định, vậy làm sao để biết khi chỉ có ArrayByte[] tập hợp
+    //         Không thể dùng Length vì nó không có gì hết
+    //              _Vì chỉ có Array[] nên bắt buộc phải có Length của string trong đây
+    //         Vậy khi GetBytes 
---
+++ M-ng-main/2212366_BTHieu_Lab4/TCPClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace TCPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient sockClient = new TcpClient("127.0.0.1", 6174);
+            Console.WriteLine("Connecting to sever ");
+            //Thiết lập kết nối 
+
+            //Khai báo dữ liệu
+            NetworkStream ns = sockClient.GetStream();
+            byte[] buffer = new byte[1024];
+            int recv;
+
+            //Nhận và gửi dữ liệu cố định 
+            recv = ns.Read(buffer, 0, buffer.Length);
+            string hello = Encoding.ASCII.GetString(buffer, 0, recv);
+            Console.WriteLine(hello);
+
+            // Gửi nhiều thông tin
+            while (true)
+            {
+                buffer = new byte[1024];
+                recv = ns.Read(buffer, 0, buffer.Length);
+                if (recv == 0)
+                {
+                    break;
+                }
+                string chuoi = Encoding.ASCII.GetString(buffer, 0, recv);
+                Console.WriteLine(chuoi);
+            }
+            Console.WriteLine("Disconnecting from sever...........");
+            ns.Close();
+            sockClient.Close();
+
+            
+        }
+    }
+}
